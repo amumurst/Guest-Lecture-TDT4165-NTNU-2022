1763148f20bd169c63c16d8f85d2246d988ff89d
Amund Murstad <amund.murstad@finn.no>
2022-10-03 14:30:01 +0200

    Add rest of intros to git

--- src/main/scala/e_1_types/_Intro.scala
+++ src/main/scala/e_1_types/_Intro.scala
@@ -35,4 +35,38 @@ object _Intro {
 
   val bob  = Person("Bob", Cat("Kit"))
   val john = bob.copy(name = "John")
+
+  /*
+    Opaque types
+   */
+
+  def saveUserForAd(userId: Long, adId: Long): Unit = ???
+
+  val aId = 1
+  val uId = 9
+
+  saveUserForAd(aId, uId)
+  saveUserForAd(adId = aId, userId = uId)
+
+  type MyAdId   = Long
+  type MyUserId = Long
+  def saveUserForAd2(userId: MyUserId, adId: MyAdId): Unit = ???
+  saveUserForAd2(aId, uId) //... still works
+
+  opaque type OAdId   = Long
+  opaque type OUserId = Long
+  def saveUserForAd3(userId: OUserId, adId: OAdId): Unit = ???
+  //saveUserForAd3(aId, uId) // Would not compile if defined in another scope than this. In here
+  //the function still "knows" that these are long
+
+  def usePositiveNumber(somePositiveNumber: Long): Unit = ???
+
+  //Opaque types are types only the compiler sees. They help us from making mistakes, while having zero
+  //overhead runtime
+  opaque type PositiveLong = Long
+  object PositiveLong:
+    def apply(l: Long) =
+      assert(l >= 0)
+      l
+    extension (p: PositiveLong) def value = p
 }
